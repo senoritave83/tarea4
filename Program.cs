ec1e57d71190e2018e0e20c428a80225b059f6c0
senoritave83 <98922151+senoritave83@users.noreply.github.com>
2022-09-28 17:29:34 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,14 @@
-﻿/*Escribe un programa que realice estos pasos:
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("TABLA MULTIPLICAR!");
+int num1 = 0;
+int tabla= 0;
+int resultado = 0;
 
-Reciba al menos un número por consola
-
-Determine si el número es positivo o negativo
-SS
-Presente un contador para cada tipo (positivo y negativo).
-    */
-float num;
-Console.WriteLine("Escriba un numero");
-num = Convert.ToInt32(Console.ReadLine());
-
-
-    int positivo = 1;
-    int negativo = 0;
+for (tabla = 1; tabla <= 10; tabla++)
+{
+    num1 = 1;
+    resultado = num1 * tabla;
+    Console.WriteLine(num1 + " X " + tabla + " = " + resultado);
    
-
-    if (num == 0)
-    {
-        Console.Write("El numero digitado {0} es cero", num);
-    }
-    if (num > 0)
-    {
-        Console.Write("El numero {0} es positivo", num);
-        positivo++;
-    }
-    if (num < 0)
-    {
-        Console.Write("El numero {0} es negativo", num);
-        negativo++;
-    }
-
-    Console.WriteLine("\ncontador de positivos  :" + positivo);
-    Console.WriteLine("contador de negativos  :" + negativo);
-    Console.ReadKey();
+   
+}
